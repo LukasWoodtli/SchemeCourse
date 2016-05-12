2ab1241b27a6bde4da836c7710dc1086492a78a3
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-05-12 21:31:06 +0200

    Add example for list processing with CAR and CDR

---
+++ 3a/list.rkt
@@ -0,0 +1,13 @@
+(DEFINE 1-to-4 (LIST 1 2 3 4))
+
+
+(CAR 1-to-4)
+
+(CAR (CDR 1-to-4))
+
+(CDR 1-to-4)
+
+(CDR (CDR 1-to-4))
+
+(CDR (CDR (CDR (CDR 1-to-4))))
+;(CDR (CDR (CDR (CDR (CDR 1-to-4)))))
