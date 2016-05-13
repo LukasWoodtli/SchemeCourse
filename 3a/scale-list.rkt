0231bc86b0e83d89380250c0d74a9fb9f27c3b49
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-05-13 09:12:52 +0200

    Add example of scale-list

---
+++ 3a/scale-list.rkt
@@ -0,0 +1,11 @@
+;; CDR-ing down a list
+
+(define (scale-list scalar lst)
+  (if (null? lst)
+      '()
+      (cons (* (car lst) scalar)
+             (scale-list scalar (cdr lst)))))
+
+
+(scale-list 2 (list 1 2 3))
+
