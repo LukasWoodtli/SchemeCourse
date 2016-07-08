c4f88f3f94aaba5ed85aa0a0430b84e6c64977b6
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-07-08 23:50:28 +0200

    Port exercises to use RackUnit

--- BookExercises/chapter1/exercise_1_1.rkt
+++ BookExercises/chapter1/exercise_1_1.rkt
@@ -1,37 +1,38 @@
 #lang sicp
 
-(define (assert-equal a b) (eqv? a b))
-
-(assert-equal 10 10)
-
-(assert-equal (+ 5 3 4) 12)
+(#%require rackunit)
 
 
+(check-equal? 10 10)
+
+(check-equal? (+ 5 3 4) 12)
 
 
-(assert-equal (- 9 1) 8)
-(assert-equal (/ 6 2) 3)
-(assert-equal (+ (* 2 4) (- 4 6)) 6)
+
+
+(check-equal? (- 9 1) 8)
+(check-equal? (/ 6 2) 3)
+(check-equal? (+ (* 2 4) (- 4 6)) 6)
 
 
 (define a 3)
 (define b (+ a 1))
 
-(assert-equal (+ a b (* a b)) 19)
-(assert-equal (= a b) #f)
+(check-equal? (+ a b (* a b)) 19)
+(check-equal? (= a b) #f)
 
-(assert-equal (if (and (> b a) (< b (* a b)))
+(check-equal? (if (and (> b a) (< b (* a b)))
     b
     a) 4)
 
-(assert-equal (cond ((= a 4) 6)
+(check-equal? (cond ((= a 4) 6)
       ((= b 4) (+ 6 7 a))
       (else 25))
               16)
 
-(assert-equal (+ 2 (if (> b a) b a)) 6)
+(check-equal? (+ 2 (if (> b a) b a)) 6)
 
-(assert-equal (* (cond ((> a b) a)
+(check-equal? (* (cond ((> a b) a)
                        ((< a b) b)
                        (else -1))
                  (+ a 1))
--- BookExercises/chapter1/exercise_1_2.rkt
+++ BookExercises/chapter1/exercise_1_2.rkt
@@ -1,7 +1,9 @@
 #lang sicp
 
+(#%require rackunit)
 
-(eqv? (/ -37 150) 
+
+(define exercise_1_2_expression
   (/ (+ 5
         4
         (- 2(- 3 (+ 6 (/ 4 5)))))
@@ -9,3 +11,5 @@
         (- 6 2)
         (- 2 7)))
 )
+
+(check-equal? (/ -37 150) exercise_1_2_expression)
