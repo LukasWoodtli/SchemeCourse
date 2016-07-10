63d84a7152bb9d02d56959751a6afec54d39264b
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-07-10 19:40:41 +0200

    Fix

--- BookExercises/chapter1/exercise_1_7.rkt
+++ BookExercises/chapter1/exercise_1_7.rkt
@@ -15,7 +15,7 @@
         (/ (+ a b) 2))
         
 (define (good-enough? guess x)
-        (< (abs (- (square guess) x)) 0.001))
+        (< (abs (- (* guess guess) x)) 0.001))
 
 (define (sqrt x) (sqrt-iter 1.0 x))
 
