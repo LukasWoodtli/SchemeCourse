3172b4e07f7973dd45adf8043ad4103ea491872f
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-07-11 21:18:26 +0200

    More tests for ackermann function (ex. 1.10)

--- BookExercises/chapter1/exercise_1_10.rkt
+++ BookExercises/chapter1/exercise_1_10.rkt
@@ -10,3 +10,6 @@
                          (ackermann x (- y 1))))))
 
 (check-equal? (ackermann 1 2) 4)
+(check-equal? (ackermann 1 10) 1024)
+(check-equal? (ackermann 2 4) 65536)
+(check-equal? (ackermann 3 3) 65536)
