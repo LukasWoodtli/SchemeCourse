ed15386b5363cf594f791a88925b9d77d86ca31c
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-07-11 21:28:43 +0200

    Improve exercise 1.11

--- BookExercises/chapter1/exercise_1_11.rkt
+++ BookExercises/chapter1/exercise_1_11.rkt
@@ -5,14 +5,27 @@
 ;; f(n) = n if n<3
 ;; and f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n>= 3
 
-(define (f n)
+(define (f-recursive n)
   (if (< n 3)
       n
-      (+ (f (dec n)) (* 2 (f (- n 2))) (* 3 (f (- n 3))))))
-  
+      (+ (f-recursive (dec n)) (* 2 (f-recursive (- n 2))) (* 3 (f-recursive (- n 3))))))
 
-(check-equal? (f 0) 0)
-(check-equal? (f 1) 1)
-(check-equal? (f 2) 2)
-(check-equal? (f 3) 4)
-(check-equal? (f 4) 11)
+;; taken from http://community.schemewiki.org/?sicp-ex-1.11
+(define (f-iterative n)
+  (define (iter a b c count)
+    (if (= count 0)
+        a
+        (iter b c (+ c (* 2 b) (* 3 a)) (- count 1))))
+  (iter 0 1 2 n))
+
+(check-equal? (f-recursive 0) 0)
+(check-equal? (f-recursive 1) 1)
+(check-equal? (f-recursive 2) 2)
+(check-equal? (f-recursive 3) 4)
+(check-equal? (f-recursive 4) 11)
+
+(check-equal? (f-iterative 0) 0)
+(check-equal? (f-iterative 1) 1)
+(check-equal? (f-iterative 2) 2)
+(check-equal? (f-iterative 3) 4)
+(check-equal? (f-iterative 4) 11)
