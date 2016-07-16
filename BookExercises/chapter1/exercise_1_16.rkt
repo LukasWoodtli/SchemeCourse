1fbc7efd249c5ba9eb6895af5b9c1c2e83bb9d76
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-07-16 15:02:09 +0200

    Finish exercise 1.16

---
+++ BookExercises/chapter1/exercise_1_16.rkt
@@ -0,0 +1,24 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (even? n)
+        (= (remainder n 2) 0))
+
+(define (square n)
+        (* n n))
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt (/ n 2))))
+        (else (* b (fast-expt n (- n 1))))))
+
+
+(check-equal? (fast-expt 1 0) 1)
+(check-equal? (fast-expt 1 1) 1)
+(check-equal? (fast-expt 1 2) 1)
+(check-equal? (fast-expt 2 2) 4)
+(check-equal? (fast-expt 3 2) 9)
+(check-equal? (fast-expt 2 3) 8)
+(check-equal? (fast-expt 5 2) 25)
+(check-equal? (fast-expt 11 11) 121)
