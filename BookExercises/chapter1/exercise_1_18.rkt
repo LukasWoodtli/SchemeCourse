a8cd908ed79d75dc9077e8ad06e89f0e5c49a958
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-07-24 17:34:52 +0200

    Add possible solution for exercise 1.18

---
+++ BookExercises/chapter1/exercise_1_18.rkt
@@ -0,0 +1,32 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (even? n)
+        (= (remainder n 2) 0))
+
+(define (double a)
+  (+ a a))
+
+(define (halve b)
+  (floor (/ b 2)))
+
+(define (fast-mult-iter a b)
+  (mult-iter a b 0))
+  
+(define (mult-iter a b accu)
+  (cond ((= b 0) accu)
+        ((even? b) (mult-iter (double a) (halve b) accu))
+        (else (+ a (fast-mult a (dec b) accu)))))
+
+(check-equal? (fast-mult-iter 2 3) 6)
+(check-equal? (fast-mult-iter 2 4) 8)
+(check-equal? (fast-mult-iter 2 5) 10)
+(check-equal? (fast-mult-iter  3 2) 6)
+(check-equal? (fast-mult-iter 3 3) 9)
+(check-equal? (fast-mult-iter 3 0) 0)
+(check-equal? (fast-mult-iter 0 0) 0)
+(check-equal? (fast-mult-iter 0 5) 0)
+(check-equal? (fast-mult-iter 1 1) 1)
+(check-equal? (fast-mult-iter 1 35) 35)
+(check-equal? (fast-mult-iter 96 1) 96)
