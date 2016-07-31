eb333083e2bf2fa368c253d2f6c400ebd1ff22f5
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-07-31 19:54:53 +0200

    Add exercise 1.35

---
+++ BookExercises/chapter1/exercise_1_35.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (golden-ratio x) (+ 1.0 (/ 1 x)))
+
+
+(display (fixed-point golden-ratio 1))
