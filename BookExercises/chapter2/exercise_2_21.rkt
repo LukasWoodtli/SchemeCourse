4facb46555819a908b7b7070f0614d52f4adbf8a
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-08-16 22:47:33 +0200

    Implement exercise 2.21

---
+++ BookExercises/chapter2/exercise_2_21.rkt
@@ -0,0 +1,19 @@
+#lang SICP
+
+
+(#%require rackunit)
+
+
+(define (square-list1 items)
+  (if (null? items)
+      nil
+      (cons (* (car items) (car items)) (square-list1 (cdr items)))))
+
+(check-equal? (square-list1 (list 1 2 3 4))
+'(1 4 9 16))
+
+(define (square-list2 items)
+ (map (lambda (a) (* a a)) items))
+
+(check-equal? (square-list2 (list 1 2 3 4))
+'(1 4 9 16))
