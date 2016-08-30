a2fac5dc560b3a5d6ce51ada4edeb8b0eb3c5807
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-08-30 10:57:51 +0200

    Add example of scaling a tree

--- BookExercises/chapter2/examples.rkt
+++ BookExercises/chapter2/examples.rkt
@@ -78,4 +78,21 @@
 
 (check-equal? (count-leaves (list x x)) 8)
                                
-         
+
+;; scale a tree by a factor
+(define (scale-tree tree factor)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (* tree factor))
+        (else (cons (scale-tree (car tree) factor)
+                    (scale-tree (cdr tree) factor)))))
+
+(check-equal? (scale-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)) 10) '(10 (20 (30 40) 50) (60 70)))
+
+(define (scale-tree-with-map tree factor)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (scale-tree-with-map sub-tree factor)
+             (* sub-tree factor)))
+       tree))
+
+(check-equal? (scale-tree-with-map (list 1 (list 2 (list 3 4) 5) (list 6 7)) 10) '(10 (20 (30 40) 50) (60 70)))
