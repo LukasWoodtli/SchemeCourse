3834761afb94042c6cd7d78ef8ce456f5a657c6f
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-10-30 21:23:35 +0100

    Add changes that were forgotten by last commit

--- BookExercises/chapter2/exercise_2_40.rkt
+++ BookExercises/chapter2/exercise_2_40.rkt
@@ -13,11 +13,56 @@
 (define (flatmap f seq)
   (accumulate append nil (map f seq)))  
 
-(define (enumerate n m)
+(define (enumerate-interval n m)
   (if (< m n)
       nil
-      (cons n (enumerate (+ 1 n) m))))
+      (cons n (enumerate-interval (+ 1 n) m))))
 
-(define (unique-pair n) 
+(define (unique-pair n)
+  (flatmap (lambda (a)
+         (map (lambda (b)
+                (list a b))
+              (enumerate-interval 1 (- a 1))))
+         (enumerate-interval 1 n)))
+              
 
-(check-equal? (reverse-b (list 1 2 3)) '(3 2 1))
+(check-equal? (unique-pair 3) '((2 1)(3 1)(3 2)))
+
+
+
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (square s)
+  (* s s))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+  
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+  
+(define (prime-sum? p)
+  (prime? (+ (car p) (cadr p))))
+
+(define (prime-sum-pairs n)
+  (map (lambda (p) (list (car p) (cadr p) (+ (car p) (cadr p))))
+       (filter prime-sum?
+          (unique-pair n))))
+
+
+
+(check-equal? (prime-sum-pairs 6) '((2 1 3)(3 2 5)(4 1 5)(4 3 7)(5 2 7)(6 1 7)(6 5 11)))
