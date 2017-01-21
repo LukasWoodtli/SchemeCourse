91f39fcf6ef6efe213cfeb21db9d152c38ef8709
Lukas Woodtli (Travis CI) <woodtli.lukas@gmail.com>
2017-01-21 22:30:53 +0100

    Try to fix racket install

--- .install-racket.sh
+++ .install-racket.sh
@@ -1,32 +1,16 @@
-# IMPORTANT: Your .travis.yml must pipe this to bash (not to sh)!
+racket-${RACKET_VERSION}.shracket-${RACKET_VERSION}.sh# IMPORTANT: Your .travis.yml must pipe this to bash (not to sh)!
 # In the Travis CI environment a #!/bin/bash shebang here won't help.
 
 set -e
 
-if [[ "$RACKET_VERSION" = "HEAD" ]]; then
-    URL="http://plt.eecs.northwestern.edu/snapshots/current/installers/racket-test-current-x86_64-linux-precise.sh"
-elif [[ "$RACKET_VERSION" = "SCOPE_SNAPSHOT" ]]; then
-    URL="http://www.cs.utah.edu/~mflatt/tmp/scope-snapshot/installers/racket-current-x86_64-linux.sh"
-elif [[ "$RACKET_VERSION" = "RELEASE" ]]; then
-    URL="http://pre-release.racket-lang.org/installers/racket-current-x86_64-linux-ubuntu-precise.sh"
-elif [[ "$RACKET_VERSION" = 5.9* ]]; then
-    URL="http://download.racket-lang.org/installers/${RACKET_VERSION}/racket-${RACKET_VERSION}-x86_64-linux-ubuntu-quantal.sh"
-elif [[ "$RACKET_VERSION" = 6.[0-4]* ]]; then
-#    URL="http://download.racket-lang.org/installers/${RACKET_VERSION}/racket-${RACKET_VERSION}-x86_64-linux-ubuntu-precise.sh"
-    URL="https://www.cs.utah.edu/plt/installers/${RACKET_VERSION}/racket-${RACKET_VERSION}-x86_64-linux-ubuntu-precise.sh"
-elif [[ "$RACKET_VERSION" = 6.* ]]; then
-    URL="http://download.racket-lang.org/installers/${RACKET_VERSION}/racket-${RACKET_VERSION}-x86_64-linux.sh"
-else
-    URL="http://download.racket-lang.org/installers/${RACKET_VERSION}/racket/racket-${RACKET_VERSION}-bin-x86_64-linux-debian-squeeze.sh"
-fi
-
+URL="https://mirror.racket-lang.org/installers/6.7/racket-6.7-x86_64-linux.sh"
 # Older .travis.yml files don't set $RACKET_DIR (the Racket install
 # directory) explicitly and expect it to be /usr/racket.
 if [[ "$RACKET_DIR" = "" ]]; then
     RACKET_DIR=/usr/racket
 fi
 
-INSTALLER="./racket-${RACKET_VERSION}.sh"
+INSTALLER="./racket-6.7-x86_64-linux.sh"
 
 echo "Downloading $URL to $INSTALLER:"
 curl -L -o $INSTALLER $URL
