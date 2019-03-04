1381b3ecdec46db941a78b4678a1bb47c1db14ad
Lukas Woodtli <woodtli.lukas@gmail.com>
2019-03-04 21:59:18 +0100

    Add some exercises

---
+++ BookExercises/chapter4/exercise_3_51_3_53.rkt
@@ -0,0 +1,102 @@
+#lang sicp
+
+(define (stream-car stream)
+  (car stream))
+(define (stream-cdr stream)
+  (force (cdr stream)))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc
+                    (map stream-cdr
+                         argstreams))))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin
+        (proc (stream-car s))
+        (stream-for-each proc
+                         (stream-cdr s)))))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1)
+                                  high))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream)
+         the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream
+          (stream-car stream)
+          (stream-filter
+           pred
+           (stream-cdr stream))))
+        (else (stream-filter
+               pred
+               (stream-cdr stream)))))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+;; Display
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+(define (show x)
+  (display-line x)
+  (newline)
+  x)
+
+;; Tests
+
+;; 3.51
+(define x
+  (stream-map
+   show
+   (stream-enumerate-interval 0 10)))
+(stream-ref x 5)
+(newline)
+(stream-ref x 7)
+
+;; 3.52
+(define sum 0)
+(define (accum x)
+  (set! sum (+ x sum))
+  sum)
+
+(define seq
+  (stream-map
+   accum
+   (stream-enumerate-interval 1 20)))
+
+(define y (stream-filter even? seq))
+
+(define z
+  (stream-filter
+   (lambda (x)
+     (= (remainder x 5) 0)) seq))
+
+(stream-ref y 7)
+(display-stream z)
+
+
+;; 3.53
+(define s (cons-stream 1 (add-streams s s)))
