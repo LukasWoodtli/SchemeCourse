6ecc574fd491cbd076d02fd35f76edcc54f2206f
Lukas Woodtli <woodtli.lukas@gmail.com>
2019-03-04 12:23:29 +0100

    Add exercise 3.50

---
+++ BookExercises/chapter4/exercise_3_50.rkt
@@ -0,0 +1,49 @@
+#lang sicp
+
+(define (stream-car stream)
+  (car stream))
+(define (stream-cdr stream)
+  (force (cdr stream)))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin
+        (proc (stream-car s))
+        (stream-for-each proc
+                         (stream-cdr s)))))
+
+
+;; Display
+(define (display-stream s)
+  (stream-for-each display-line s))
+(define (display-line x)
+  (newline)
+  (display x))
+
+;; stream-map
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc
+                    (map stream-cdr
+                         argstreams))))))
+
+;; Tests
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1)
+                                  high))))
+
+;; Print result
+(display-stream (stream-map (lambda (x) (* 2 x)) (stream-enumerate-interval 5 10)))
